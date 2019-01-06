5dbef25aac52927f33fa235dbd3b622f1cc9de40
James Luck <djluck20@googlemail.com>
2019-01-06 15:56:03 +1100

    Started work on README, added examples (verified server GC bug does affect collected metrics)

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # prometheus-net.DotNetMetrics
+A plugin for the [prometheus-net](https://github.com/prometheus-net/prometheus-net) package, exposing .NET core runtime metrics including:
+- Garbage collection frequencies, pauses and timings
+- Heap size by generation
+- Bytes allocated
+- JIT compilations
+- Thread pool size, scheduling delays and reasons for growing/ shrinking
+- Lock contention
+
+These metrics are essential for understanding the peformance of any non-trivial application. Even if your application is well instrumented, you're only getting half the story- what the runtime is doing completes the picture.
+
+# Installation
+_Requires .NET core v2.2+_.
+
+Add the packge using:
+```
+dotnet add package prometheus-net.DotNetRuntime --version 0.0.4-alpha
+```
+
+And then register the collector:
+```
+ DefaultCollectorRegistry.Instance.RegisterOnDemandCollectors(DotNetRuntimeStatsBuilder.Default());
+```
+
+
+
---
+++ examples/AspNetCoreExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Prometheus;
+using Prometheus.Advanced;
+using Prometheus.DotNetRuntime;
+
+namespace AspNetCoreExample
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            DefaultCollectorRegistry.Instance.RegisterOnDemandCollectors(DotNetRuntimeStatsBuilder.Default());
+            var metricServer = new MetricServer(12203);
+            metricServer.Start();
+            
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
