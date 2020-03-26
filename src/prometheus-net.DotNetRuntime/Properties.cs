ac7f809ef037c15fae79250af1dfad7f48cf1bc6
Rama Krishnam Raju Potturi <rapottu@microsoft.com>
2020-03-26 04:26:08 -0700

    remove public key

--- src/prometheus-net.DotNetRuntime/Properties.cs
+++ src/prometheus-net.DotNetRuntime/Properties.cs
@@ -1,6 +1,2 @@
 ﻿using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("prometheus-net.DotNetRuntime.Tests, PublicKey=00240000048000009400000006020000" +
-                             "00240000525341310004000001000100a16035bee2b01495f350b4af8a1d0b6fc44ef62b773f91b" +
-                             "5eec722eb1f1153ca68af198750bba0397bec986abae2f504ec94b729eb6ba81f059220f509f49f4" +
-                             "b2b8543a3882e2714ebe640122705b682a577ecf7458249a88ca91eb6a1dd2274462821a8060c6e0" +
-                             "c8e28b732953b197b546450e774430949ac3d114db8f8edbc")]
+[assembly: InternalsVisibleTo("prometheus-net.DotNetRuntime.Tests")]
